2b394f1fedf0d22dc4600e91f3d5ac350fcf8c7b
Bsneal <BrendanSNeal@gmail.com>
2020-02-27 15:35:59 -0700

    Initial Commit

---
+++ Assignment 2/src/cse360assign2/SimpleList.java
@@ -0,0 +1,114 @@
+package cse360assign2;
+
+/**
+ * @author Brendan Neal; class ID 123; Assignment 1
+ */
+
+/**
+ * This class defines the functionality for a simple list. This list
+ * has a maximum of 10 values which are justified so that the empty
+ * section of the array is to the right.
+ * 
+ * @param	list	the array holding the elements of the list
+ * @param	count	a parameter that keeps track of how many elements are in the array
+ */
+class SimpleList {
+	private int[] list;
+	private int count;
+	
+	/**
+	 * The constructor for the class. This instantiates the
+	 * list and count parameters. The size of the array is set to 10
+	 * and the count is set to 0;
+	 */
+	public SimpleList(){
+		list = new int[10];
+		count = 0;
+	}
+	
+	/**
+	 * Adds an element to the head of list. In doing so, it pushes
+	 * all other array elements are pushed to the right of the array.
+	 * If the list is full, the last element is pushed off the array.
+	 * 
+	 * @param value	The value that is added to the array.
+	 */
+	public void add(int value) {
+		for(int increment = count - 1; increment >= 0; increment--) {
+			if (increment != 9) {
+				list[increment + 1] = list[increment];
+			}
+		}
+		list[0] = value;
+		if(count < 10) count++;
+	}
+	
+	/**
+	 * Removes the specified element from the array if it exists.
+	 * If a value is removed, all values ahead of the removed
+	 * element are moved back to fill the gap left.
+	 * 
+	 * It should be noted that this method doesn't delete old data.
+	 * It's assumed that because the count parameter defines the
+	 * size of the list, any values past the count will be unreachable.
+	 * 
+	 * @param value	Value to be removed if it exists in the list
+	 * @return	does not return a value. Return is used as a method
+	 * 			to escape the method.
+	 */
+	public void remove(int value) {
+		int pos = search(value);
+		if(pos == -1) {
+			return;
+		}
+		for(int increment = pos + 1; increment < count; increment++) {
+			list[increment] = list[increment - 1];
+		}
+		count--;
+	}
+	
+	/**
+	 * Method that returns the number of elements in the list.
+	 * 
+	 * @return count, which is the number of elements in the list.
+	 */
+	public int count() {
+		return count;
+	}
+	
+	/**
+	 * A method that outputs the list as a string. The string cannot
+	 * end in a space, so trim() is used to remove unnecessary spaces.
+	 */
+	public String toString() {
+		String outString = "";
+		
+		for(int increment = 0; increment < count; increment++) {
+			outString += list[increment] + " ";
+		}
+		
+		outString = outString.trim();
+		
+		return outString;
+	}
+	
+	/**
+	 * Searches for a given value in the list and returns the index 
+	 * of the value in the list if it is found. Returns -1 otherwise.
+	 * 
+	 * @param value	The value this method searches for
+	 * @return	returns the index of the value or -1
+	 */
+	int search(int value) {
+		int pos = -1;
+		int increment = count;
+		while(increment >= 0) {
+			if(list[increment] == value) {
+				pos = increment;
+			}
+			increment--;
+		}
+		return pos;
+	}
+	
+}
